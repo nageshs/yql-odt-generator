31c82235616078e98eb53ff6b8d1b777751e5895
Srinagesh Susarla <nageshs@streetshout-lm.(none)>
2009-06-18 19:21:14 -0700

    First cut: YQL Open Data Table Generator. Generates ODT using a CLI interface

---
+++ src/main/java/net/nagiworld/odtgen/Main.java
@@ -0,0 +1,23 @@
+package net.nagiworld.odtgen;
+
+import com.sampullara.cli.Args;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: nageshs
+ * Date: Jun 18, 2009
+ * Time: 3:00:04 PM
+ */
+public class Main {
+
+
+  public static void main(String[] args) {
+    OpenDataTableBean odtBean = new OpenDataTableBean();    
+    if (args.length == 0) {
+      Args.usage(odtBean);
+      return;
+    }
+    Args.parse(odtBean, args);
+    System.out.println(odtBean.toXMLString());
+  }
+}
---
+++ src/main/java/net/nagiworld/odtgen/OpenDataTableBean.java
@@ -0,0 +1,109 @@
+package net.nagiworld.odtgen;
+
+import com.sampullara.cli.Argument;
+import org.antlr.stringtemplate.StringTemplate;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: nageshs
+ * Date: Jun 18, 2009
+ * Time: 5:46:51 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class OpenDataTableBean {
+
+  private static final String template;
+  private static final String LINE_SEP = System.getProperty("line.separator");
+
+  private static final Set<String> ALLOWED_BINDINGS =
+          new HashSet<String>(Arrays.asList("insert", "update", "delete", "select"));
+  
+  private String binding;
+  private String author;
+  private String sampleQuery;
+  private String itemPath;
+  private StringTemplate st;
+
+  public OpenDataTableBean() {
+    st = new StringTemplate(template);
+  }
+
+  public String getAuthor() {
+    if (author == null) author = System.getProperty("user.name");
+    return author;
+  }
+
+  @Argument (description = "Sets the Author of the ODT. Defaults to user.name ")
+  public void setAuthor(String author) {
+    this.author = author;
+  }
+
+  public String getBinding() {
+    return binding;
+  }
+
+  @Argument (required = true, alias = "b", description="Binding such as insert, update, select, delete")
+  public void setBinding(String binding) {
+    binding = binding.toLowerCase();
+    this.binding = binding;
+  }
+
+  public String getItemPath() {
+    return itemPath;
+  }
+  @Argument (description = "itemPath associated with the binding")
+  public void setItemPath(String itemPath) {
+    this.itemPath = itemPath;
+  }
+
+  public String getSampleQuery() {
+    return sampleQuery;
+  }
+  @Argument (alias = "sample", description = "A sample Query for the binding")
+  public void setSampleQuery(String sampleQuery) {
+    this.sampleQuery = sampleQuery;
+  }
+
+  public String toXMLString() {
+    if (!ALLOWED_BINDINGS.contains(binding)) {
+      throw new IllegalArgumentException("Invalid binding '" + binding +
+              "'. Expecting one of "+ALLOWED_BINDINGS);
+    }
+    st.setAttribute("binding", getBinding());
+    st.setAttribute("author", getAuthor());
+    st.setAttribute("sampleQuery", getSampleQuery());
+    st.setAttribute("itemPath", getItemPath());
+    return st.toString();
+  }
+
+
+  static {
+    try {
+      template = makeTemplate();
+    } catch (IOException e) {
+      throw new AssertionError(e);
+    }
+  }
+
+  private static String makeTemplate() throws IOException {
+    //System.out.println(new Object().getClass().getResource("/net/nagiworld/odtgen/templates/template.st"));
+    InputStream is = Main.class.getResourceAsStream("/net/nagiworld/odtgen/templates/template.st");
+    BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+    String line = null;
+    StringBuilder sb = new StringBuilder();
+    while ((line = reader.readLine()) != null) {
+      sb.append(line);
+      sb.append(LINE_SEP);
+    }
+    return sb.toString();
+  }
+
+}
